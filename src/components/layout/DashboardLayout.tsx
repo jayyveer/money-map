a58c66530a2884302bd0957f93af44f7589db7bf
Jayveer <jayveersinghrajawat98@gmail.com>
2025-05-12 02:31:04 +0530

    removed debt section

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -151,15 +151,6 @@ export function DashboardLayout() {
                 <span className="ml-3">Reports & Analytics</span>
               </a>
             </li>
-            <li>
-              <a
-                href="/dashboard/debt"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <TrendingDown className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                <span className="ml-3">Debt Management</span>
-              </a>
-            </li>
           </ul>
         </div>
       </aside>
